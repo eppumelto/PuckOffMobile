a7f7377934bf4272200b8ee7e90032a31bc99d6b
eppumelto <eppu.melto@hotmail.fi>
2022-01-11 14:27:54 +0200

    no message

---
+++ puckoffmobiledemo/Assets/Scripts/HealthbarScript_REMOTE_2098.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarScript : MonoBehaviour
+{
+    public Slider slider;
+    public float hp = 100;
+
+
+    // Pelaajan maksimi health
+    public void MaxHealth (int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    public void Update()
+    {
+        slider.value = hp; //paivittaa hp mittaria
+    }
+
+}
