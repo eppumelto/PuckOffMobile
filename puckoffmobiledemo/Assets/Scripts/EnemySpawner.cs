c2474ed98c1bfa470eb1e4fb2488d87f7b3d215b
mollaaja <jesse1.pirinen@gmail.com>
2022-02-10 09:41:15 +0200

    epulle

--- puckoffmobiledemo/Assets/Scripts/EnemySpawner.cs
+++ puckoffmobiledemo/Assets/Scripts/EnemySpawner.cs
@@ -35,7 +35,7 @@ public class EnemySpawner : MonoBehaviour
         else if ( TakeDmg.enemiesKilled == 1 && TakeDmg.isAlive == false && TakeDmg.PlayerAlive)
         {
             BossTxt.SetActive(true);
-            objectPooler.SpawnFromPool("Boss",new Vector3(transform.position.x,transform.position.y, transform.position.z), Quaternion.identity);
+            objectPooler.SpawnFromPool("Boss",new Vector3(transform.position.x,transform.position.y + 1.5f, transform.position.z), Quaternion.identity);
             BossSpawn.Play();
             
             
