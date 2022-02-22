e056f8718904ed4083383cd9f95f39d67b4d20b5
Aaro Luomala <bomberplayer@hotmail.com>
2022-02-22 08:39:06 +0200

    MainMenu tavaraa, koodin muokkaus viel keske :D

--- puckoffmobiledemo/Assets/Mainemenu/Koodi/SettingsMenu.cs
+++ puckoffmobiledemo/Assets/Mainemenu/Koodi/SettingsMenu.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Dropdown qualityDropDown;
+    public Slider volSlider;
+
+
+    const string prefName = "optionValue";
+
+    private void Awake()
+    {
+        qualityDropDown.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+            PlayerPrefs.SetInt(prefName, qualityDropDown.value);
+            PlayerPrefs.Save();
+        }));
+    }
+    private void Start()
+    {
+        volSlider.value = PlayerPrefs.GetFloat("MVolume", 1f);
+        audioMixer.SetFloat("Volume", PlayerPrefs.GetFloat("MVolume"));
+
+        qualityDropDown.value = PlayerPrefs.GetInt(prefName, 3);
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
