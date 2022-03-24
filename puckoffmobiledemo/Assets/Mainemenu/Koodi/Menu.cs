305a9dd72f1859acaad4995ea8c95f3f93222178
Jesse Pirinen <jesse1.pirinen@gmail.com>
2022-03-24 14:15:20 +0200

    commit

--- puckoffmobiledemo/Assets/Mainemenu/Koodi/Menu.cs
+++ puckoffmobiledemo/Assets/Mainemenu/Koodi/Menu.cs
@@ -12,22 +12,11 @@ public class Menu : MonoBehaviour
     public GameObject MainMenuPanel;
     private GameObject instance;
 
-    //private void Awake()
-    //{
-    //    if (instance == null)
-    //    {
-    //        instance = this.gameObject;
-    //    }
-    //    else
-    //    {
-    //        if (instance != this)
-    //        {
-    //            Destroy(gameObject);
-    //        }
-    //    }
-    //    DontDestroyOnLoad(gameObject);
 
-    //}
+
+
+  
+
 
 
     public void Resume()
